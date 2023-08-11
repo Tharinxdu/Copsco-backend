9353bfa375c3ac251e1e3e4035bb14ddeae358f4
Tharindu Attygalle <tharindud022@gmail.com>
2023-08-11 10:58:26 +0530

    fineManagement modifications

--- routes/getDriver.js
+++ routes/getDriver.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../database.js'); 
+const db = require('../db.config.js'); 
 const { get } = require('lodash');
 
 const pool = db.pool;
